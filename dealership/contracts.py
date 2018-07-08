55f241c4e8ad23460ac41f456c8f55968f9ef85b
Jessica <jessicachng@gmail.com>
2018-07-08 13:13:14 +0000

    Car Dealership assignment

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,50 @@
+from dealership.customers import Customer, Employee
+from dealership.vehicles import Car, Truck, Motorcycle
+
 class Contract(object):
     pass
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.monthly_payments = monthly_payments
+        
 
+    def total_value(self):
+
+        total_value = self.vehicle.sale_price() + (self.vehicle.interest_rate * self.monthly_payments * self.vehicle.sale_price() / 100)
+
+
+        # Also possible:
+        # if self.customer.is_employee():
+        if isinstance(self.customer, Employee):
+            total_value = 0.9 * total_value
+        
+        return round (total_value, 2)         
+        
+        
+    def monthly_value(self):
+        monthly_value = self.total_value() / self.monthly_payments    
+        return monthly_value
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        self.vehicle = vehicle
+        self.customer = customer
+        self.length_in_months = length_in_months
+    
+    def total_value(self):
+
+        total_value = self.vehicle.sale_price() + (self.vehicle.sale_price() * self.vehicle.lease_multiplier / self.length_in_months) 
+        
+        if isinstance(self.customer, Employee):
+            total_value = 0.9 * total_value
+        
+        return round (total_value, 2)
+        
+    def monthly_value(self):
+        monthly_value = self.total_value() / self.length_in_months
+        return monthly_value
+    
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,37 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker 
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
+   
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+        
+        
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multiplier * self.miles)
 
+    
 class Car(Vehicle):
-    pass
+    interest_rate = 1.07
+    lease_multiplier = 1.2
+    sale_multiplier = 1.2
+    purchase_multiplier = 0.004
 
 
 class Motorcycle(Vehicle):
-    pass
+    interest_rate = 1.03
+    lease_multiplier = 1
+    sale_multiplier = 1.1
+    purchase_multiplier = 0.009
 
 
 class Truck(Vehicle):
-    pass
+    interest_rate = 1.11
+    lease_multiplier = 1.7
+    sale_multiplier = 1.6
+    purchase_multiplier = 0.02
+
